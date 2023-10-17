d8e96a3c0c7fc4c531df3eebec47f34649aad6ab
Tara Balakrishnan <taragb@gmail.com>
2023-10-16 23:14:18 -0700

    test server side code function

--- next-app/app/new/page.tsx
+++ next-app/app/new/page.tsx
@@ -1,12 +1,18 @@
 import Link from "next/link";
 
+async function createTodo(data: FormData) {
+    "use server"
+    // this line tells us that this runs on the server
+    console.log("Hi")
+}
+
 export default function Page() {
     return (
         <>
             <header className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl">New</h1>
             </header>
-            <form className="flex gap-2 flex-col">
+            <form action={createTodo} className="flex gap-2 flex-col">
                 <input type="text" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"></input>
                 <div className="flex gap-1 justify-end">
                     <Link href=".." className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Cancel</Link>
@@ -15,4 +21,5 @@ export default function Page() {
             </form>
         </>
     )
+
 }
